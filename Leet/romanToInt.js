e1ce89346d0b6a8fcb818323660e5648c035e1c0
Jason Whisnant <tasups@gmail.com>
2023-05-13 19:04:02 +0000

    last test case failing

--- Leet/romanToInt.js
+++ Leet/romanToInt.js
@@ -47,3 +47,78 @@ s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
 */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ 
+function romanToInt (s) {
+
+  let total = 0;
+
+  for (let i = 0; i <  s.length; i++) {
+    if (s[i] == "I" && s[i + 1] == "V") {
+      total += 4;
+      i++;
+      console.log(i);
+    } else if (s[i] == "I" && s[i + 1] == "X") {
+      total += 9;
+      i++;
+      console.log(i);
+    } else if (s[i] == "X" && s[i + 1] == "L") {
+      total += 40;
+      i++;
+      console.log(i);
+    } else if (s[i] == "X" && s[i + 1] == "C") {
+      total += 90;
+      i++;
+      console.log(i);
+    } else if (s[i] == "C" && s[i + 1] == "D") {
+      total += 400;
+      i++;
+      console.log(i);
+    } else if (s[i] == "C" && s[i + 1] == "M") {
+      total += 900;
+      i++;
+      console.log(i);
+    } 
+    
+    switch (s[i]) {
+      case 'I':
+        total++;
+        break;
+      case 'V':
+        total += 5;
+        break;
+      case 'X':
+        total += 10;
+        break;
+      case 'L':
+        total += 50;
+        break;
+      case 'C':
+        total += 100;
+        break;
+      case 'D':
+        total += 500;
+        break;
+      case 'M':
+        total += 1000 ;
+        break;
+      default:
+        break;
+    }
+    
+  }
+  console.log(total);
+  return total;
+    
+};
+
+// TEST CASES
+
+romanToInt("III") // -> Output: 3
+
+romanToInt("LVIII") // -> Output: 58
+
+romanToInt("MCMXCIV") // -> Output: 1994
