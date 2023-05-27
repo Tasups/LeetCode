d59d7b261edff98118c668aade9a2f6a325b7d72
Jason Whisnant <tasups@gmail.com>
2023-05-27 17:43:18 -0400

    first section of codesignal arcade complete

---
+++ CodeSignal/theJourneyBegins.js
@@ -0,0 +1,35 @@
+// This is the first phase in the arcade version of CodeSignal
+
+// function solution(year) {
+//   const result = year / 100;
+//   if (result % 1 > 0) {
+//     return Math.floor(result + 1);
+//   }
+//   return result;
+// }
+
+// console.log(solution(1988))
+// console.log(solution(1500))
+// console.log(solution(200))
+
+// function solution(inputString) {
+//   let strToArr = [];
+//   for (let index = 0; index < inputString.length; index++) {
+//     strToArr.push(inputString[index]);
+//   }
+//   const reversedArr = strToArr.reverse();
+//   const reversedString = reversedArr.join("");
+//   console.log(reversedString)
+
+//   if (inputString == reversedString) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+
+// }
+
+// console.log(solution("aabaa"));
+// console.log(solution("abac"));
+// console.log(solution("a"));
+
