df0c6272c118c7b489ef773f7b2e43743aab917f
Jason Whisnant <tasups@gmail.com>
2023-05-31 12:19:13 +0000

    statues missing places

--- CodeSignal/edgeOfTheOcean.js
+++ CodeSignal/edgeOfTheOcean.js
@@ -82,32 +82,78 @@ The area of the n-interesting polygon.
   */
 
 
-function solution(n) {
-    let result = 0;
-    if (n >= 3) {
-        let cross = 4 * (n - 1) + 1;
-        let count = n;
-        let total = 0;
-        while(count >= 3) {
-            console.log("count: " + count);
-            let web = 4 * (count - 2);
-            console.log("web: " + web);
-            total = total + web;
-            console.log("total: " + total);
-            count--;
-        }
-        result += total + cross;
-    } else if (n === 2) {
-        result = 5;
-    } else if (n === 1) {
-        result = 1;
-    }
+// function solution(n) {
+//     let result = 0;
+//     if (n >= 3) {
+//         let cross = 4 * (n - 1) + 1;
+//         let count = n;
+//         let total = 0;
+//         while(count >= 3) {
+//             console.log("count: " + count);
+//             let web = 4 * (count - 2);
+//             console.log("web: " + web);
+//             total = total + web;
+//             console.log("total: " + total);
+//             count--;
+//         }
+//         result += total + cross;
+//     } else if (n === 2) {
+//         result = 5;
+//     } else if (n === 1) {
+//         result = 1;
+//     }
     
-    return result
+//     return result
+// }
+
+// console.log(solution(1)); // 1
+// console.log(solution(2)); // 5
+// console.log(solution(3)); // 13
+// console.log(solution(4)); // 25
+// console.log(solution(5)); // 41
+
+/*
+Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+Example
+
+For statues = [6, 2, 3, 8], the output should be
+solution(statues) = 3.
+
+Ratiorg needs statues of sizes 4, 5 and 7.
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] array.integer statues
+
+An array of distinct non-negative integers.
+
+Guaranteed constraints:
+1 ≤ statues.length ≤ 10,
+0 ≤ statues[i] ≤ 20.
+
+[output] integer
+
+The minimal number of statues that need to be added to existing statues such that it contains every integer size from an interval [L, R] (for some L, R) and no other sizes.
+*/
+
+function solution (statues) {
+    const sortedStatues = statues.sort();
+    const revSortStatues = sortedStatues.reverse();
+    let toFill = 0;
+    for (let i = 0; i < revSortStatues.length; i++) {
+        if (revSortStatues[i + 1] < revSortStatues.length) {
+            toFill += (revSortStatues[i] - revSortStatues[i + 1]) - 1;
+        }
+    }
+    console.log(toFill);
+    return toFill;
 }
 
-console.log(solution(1)); // 1
-console.log(solution(2)); // 5
-console.log(solution(3)); // 13
-console.log(solution(4)); // 25
-console.log(solution(5)); // 41
+
+let statOne = [6, 2, 3, 8] // => 3.
+solution(statOne);
